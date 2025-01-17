660d121607b47511206e0a1bc4d3c5f9318e42e4
camkol <koliyus3@hotmail.com>
2025-01-17 09:06:30 -0500

    style imggen

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ImageGenerator.css";
 import default_image from "../Assets/default_image.svg";
 
 const ImageGenerator = () => {
+  const [image_url, setImage_url] = useState("/");
+
   return (
     <div className="ai-image-generator">
       <div className="header">
@@ -12,6 +14,14 @@ const ImageGenerator = () => {
         <div className="image">
           <img src={default_image} alt="" />
         </div>
+      </div>
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Describe what you want to see"
+        />
+        <div className="generate-btn">Generate</div>
       </div>
     </div>
   );

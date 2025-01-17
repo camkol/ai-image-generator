84665235c6925fcf32ce94f0675ddb36fee75de0
camkol <koliyus3@hotmail.com>
2025-01-17 11:42:51 -0500

    complete fetch, set image

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -5,12 +5,34 @@ import default_image from "../Assets/default_image.svg";
 const ImageGenerator = () => {
   const [image_url, setImage_url] = useState("/");
   let inputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
 
   const imageGenerator = async () => {
     if (inputRef.current.value === "") {
       return 0;
     }
-    const response = await fetch();
+    setLoading(true);
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_AI_GEN_API_KEY}`,
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 1,
+          size: "512x512",
+        }),
+      }
+    );
+    let data = await response.json();
+
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
+    setLoading(false);
   };
 
   return (
@@ -22,6 +44,12 @@ const ImageGenerator = () => {
         <div className="image">
           <img src={image_url === "/" ? default_image : image_url} alt="" />
         </div>
+        <div className="loading">
+          <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
+          <div className={loading ? "loading-text" : "display-none"}>
+            Loading...
+          </div>
+        </div>
       </div>
       <div className="search-box">
         <input
@@ -30,7 +58,14 @@ const ImageGenerator = () => {
           className="search-input"
           placeholder="Describe what you want to see"
         />
-        <div className="generate-btn">Generate</div>
+        <div
+          className="generate-btn"
+          onClick={() => {
+            imageGenerator();
+          }}
+        >
+          Generate
+        </div>
       </div>
     </div>
   );

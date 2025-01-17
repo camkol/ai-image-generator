59d4e627e1aa7bc1825fa302040c8f186d5400dc
camkol <koliyus3@hotmail.com>
2025-01-17 09:40:09 -0500

    prepare for fetch

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./ImageGenerator.css";
 import default_image from "../Assets/default_image.svg";
 
 const ImageGenerator = () => {
   const [image_url, setImage_url] = useState("/");
+  let inputRef = useRef(null);
+
+  const imageGenerator = async () => {
+    if (inputRef.current.value === "") {
+      return 0;
+    }
+    const response = await fetch();
+  };
 
   return (
     <div className="ai-image-generator">
@@ -12,12 +20,13 @@ const ImageGenerator = () => {
       </div>
       <div className="img-loaging">
         <div className="image">
-          <img src={default_image} alt="" />
+          <img src={image_url === "/" ? default_image : image_url} alt="" />
         </div>
       </div>
       <div className="search-box">
         <input
           type="text"
+          ref={inputRef}
           className="search-input"
           placeholder="Describe what you want to see"
         />
